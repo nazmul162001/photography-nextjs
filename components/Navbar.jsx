68d56bc74796a2ad521b8999278f0fe4f2bd8527
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-01 18:10:17 +0600

    setup navbar for mobile menu

---
+++ components/Navbar.jsx
@@ -0,0 +1,71 @@
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const [color, setColor] = useState('transparent')
+  const [textColor, setTextColor] = useState('white')
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
+        <Link href='/'>
+          <h2 className='font-bold text-4xl'>Capture</h2>
+        </Link>
+        <ul className='sm:flex hidden'>
+          <li className='p-4'>
+            <Link href='/'>Home</Link>
+          </li>
+          <li className='p-4'>
+            <Link href='/#gallery'>Gallery</Link>
+          </li>
+          <li className='p-4'>
+            <Link href='/portfolio'>Work</Link>
+          </li>
+          <li className='p-4'>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+
+        {/* Mobile Button  */}
+        <div className='block sm:hidden z-10'>
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+
+        {/* Mobile Menu  */}
+
+        <div
+          className={
+            nav
+              ? 'absolute top-0 right-0 left-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              : 'absolute top-0 right-0 left-[-100%] bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          }
+        >
+          <ul>
+            <li className='p-4 text-4xl hover:text-gray-500'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='p-4 text-4xl hover:text-gray-500'>
+              <Link href='/#gallery'>Gallery</Link>
+            </li>
+            <li className='p-4 text-4xl hover:text-gray-500'>
+              <Link href='/portfolio'>Work</Link>
+            </li>
+            <li className='p-4 text-4xl hover:text-gray-500'>
+              <Link href='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* END MENU WITH BUTTON */}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
