10275748f757c2f9ea89bd15415a2dd8b2fa605b
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-01 19:41:01 +0600

    complete portfolio section & fixed mobile navmenu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
             <Link href='/#gallery'>Gallery</Link>
           </li>
           <li className='p-4'>
-            <Link href='/portfolio'>Work</Link>
+            <Link href='/work'>Work</Link>
           </li>
           <li className='p-4'>
             <Link href='/contact'>Contact</Link>
@@ -61,16 +61,16 @@ const Navbar = () => {
           }
         >
           <ul>
-            <li className='p-4 text-4xl hover:text-gray-500'>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
               <Link href='/'>Home</Link>
             </li>
-            <li className='p-4 text-4xl hover:text-gray-500'>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
               <Link href='/#gallery'>Gallery</Link>
             </li>
-            <li className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/portfolio'>Work</Link>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
+              <Link href='/work'>Work</Link>
             </li>
-            <li className='p-4 text-4xl hover:text-gray-500'>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
               <Link href='/contact'>Contact</Link>
             </li>
           </ul>
